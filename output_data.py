afb30ec8872406a477d2dc6092b079a52ab9d039
MaikZ91 <maikz@miltenyi.com>
2024-07-04 04:51:49 +0200

    changes

--- output_data.py
+++ output_data.py
@@ -18,7 +18,7 @@ DAC.set_DAC_outrange(OUTPUT_RANGE_10V)
 #Output value from DAC channel 0
 DAC.set_DAC_out_voltage(0,1)
 # time.sleep(2)
-# DAC.set_DAC_out_voltage(5000,1)
+#DAC.set_DAC_out_voltage(5000,1)
 # time.sleep(2)
 # DAC.set_DAC_out_voltage(10000,1)
 
