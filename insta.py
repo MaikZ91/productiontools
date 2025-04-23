166da26494797853f8a308869711073263689505
MaikZ91 <maikz@miltenyi.com>
2025-04-23 23:26:14 +0200

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,30 +1,27 @@
 import requests
-from datetime import datetime
 
 ACCESS_TOKEN = "EAARGZAhh6FeUBO1BcNnupzxFKg9arIi96vbbw4oZCQfjTU7oauMqxdQaO1FzNs50nMBNSxV0rUlv4h73hOb58u4w5mCbtZAaBYJZBBHNKeYnLbSyGWXcogpoWadVj5ZBUJ5S6i4K7FJNuUIDl8dRodAtXYZCy9f1b2uB55UOrC8fUN4lCeA1YN91XEJJ0HY4PtMbzZCGOE2"
 IG_USER_ID = "17841439206364243"
+IMAGE_URL = "https://raw.githubusercontent.com/MaikZ91/productiontools/master/Unbenannt.png"
+CAPTION = "🔥 TRIBE WORKOUT – jeden Donnerstag! 💪 Kostenlos & offen für alle. #tribe #bielefeld #workout"
 
-image_url = "https://dein-server.de/pfad/zu/Unbenannt.png"  # Du brauchst hier eine echte URL!
-caption = "🔥 TRIBE WORKOUT – jeden Donnerstag! 💪 Kostenlos & offen für alle. #tribe #bielefeld #workout"
-
-
-create_url = f"https://graph.facebook.com/v19.0/{IG_USER_ID}/media"
+# 1. Instagram Medienobjekt erstellen
 media_payload = {
-    "image_url": image_url,
-    "caption": caption,
+    "image_url": IMAGE_URL,
+    "caption": CAPTION,
     "access_token": ACCESS_TOKEN
 }
-media_response = requests.post(create_url, data=media_payload)
-creation_id = media_response.json().get("id")
+media_response = requests.post(f"https://graph.facebook.com/v19.0/{IG_USER_ID}/media", data=media_payload)
+media_json = media_response.json()
+creation_id = media_json.get("id")
 
-# 2. Veröffentliche das Medienobjekt
+# 2. Medienobjekt veröffentlichen
 if creation_id:
-    publish_url = f"https://graph.facebook.com/v19.0/{IG_USER_ID}/media_publish"
     publish_payload = {
         "creation_id": creation_id,
         "access_token": ACCESS_TOKEN
     }
-    publish_response = requests.post(publish_url, data=publish_payload)
-    print(publish_response.json())
+    publish_response = requests.post(f"https://graph.facebook.com/v19.0/{IG_USER_ID}/media_publish", data=publish_payload)
+    print("✅ Erfolgreich gepostet:", publish_response.json())
 else:
-    print("Fehler beim Erstellen des Medienobjekts:", media_response.text)
+    print("❌ Fehler beim Erstellen:", media_response.text)
