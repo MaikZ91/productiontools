367cda91bd5578aaa704e31afa6c3d5572c3f828
MaikZ91 <maikz@miltenyi.com>
2025-04-24 12:38:11 +0200

    Update insta.py

--- insta.py
+++ insta.py
@@ -123,7 +123,7 @@ def main():
     caption="Weitere Events und Infos findest du in unserer App (Alle Angaben ohne Gewähr auf Richtigkeit)➡ Link in Bio 🔗\n\n" + "\n".join(f"• {e['event']}" for e in events)
     post_id=insta_post(raw_url, caption, ig_uid, ig_tok)
 
-    if weekday == 2:                         
+    if weekday == 3:                         
         caption = "Tribe Powerworkout 💪\nMehr in der Bio 🔗"
         insta_post("https://raw.githubusercontent.com/MaikZ91/productiontools/master/Unbenannt.png"
 , caption, ig_uid, ig_tok)
