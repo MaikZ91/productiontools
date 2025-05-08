b3b93fc2c56b3b03a299162399a726fe7f6c4036
MaikZ91 <maikz@miltenyi.com>
2025-05-08 10:08:57 +0200

    Update insta.py

--- insta.py
+++ insta.py
@@ -128,7 +128,7 @@ def main():
     dm_2 = datetime.now(tz).strftime("%d.%m")         # "08.05"
     dm_1 = datetime.now(tz).strftime("%-d.%m") 
     raw=requests.get(URL,timeout=10).text
-    #events=[e for e in json.loads(raw) if e.get("date","?").endswith(dm_2, dm_1) and "hochschulsport" not in e.get("event","").lower()]
+    events=[e for e in json.loads(raw) if e.get("date","?").endswith(dm_2, dm_1) and "hochschulsport" not in e.get("event","").lower()]
     #events = [e for e in json.loads(raw) if e.get("date","?").endswith(dm)]
     
 
