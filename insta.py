2057a86c994394d7726bbf7553b2ee963aa5b46c
MaikZ91 <maikz@miltenyi.com>
2025-05-24 15:47:37 +0200

    Update insta.py

--- insta.py
+++ insta.py
@@ -21,10 +21,10 @@ import re
 URL = "https://raw.githubusercontent.com/MaikZ91/productiontools/master/events.json"
 #GITHUB_VIDEO_FILE = Path(__file__).resolve().parent / "media" / "3188890-hd_1920_1080_25fps.mp4"
 #GITHUB_VIDEO_FILE = Path(__file__).resolve().parent / "media" / "bielefeldbynight.mp4"
-#GITHUB_VIDEO_FILE = Path(__file__).resolve().parent / "media" / "20250520_1822_Vibrant City Billboard_simple_compose_01jvq81fa7eywa1d4r9363dzd0.mp4"
-GITHUB_VIDEO_FILE = Path(__file__).resolve().parent / "media" / "YouCut_20250522_184722743.mp4"
-MUSIC_FILE = Path(__file__).resolve().parent / "media" / "INDEPENDENCE. (mp3cut.net).mp3"
-#MUSIC_FILE = Path(__file__).resolve().parent / "media" / "ali-safari.mp3"
+GITHUB_VIDEO_FILE = Path(__file__).resolve().parent / "media" / "20250520_1822_Vibrant City Billboard_simple_compose_01jvq81fa7eywa1d4r9363dzd0.mp4"
+#GITHUB_VIDEO_FILE = Path(__file__).resolve().parent / "media" / "YouCut_20250522_184722743.mp4"
+#MUSIC_FILE = Path(__file__).resolve().parent / "media" / "INDEPENDENCE. (mp3cut.net).mp3"
+MUSIC_FILE = Path(__file__).resolve().parent / "media" / "ali-safari.mp3"
 W, H,  PAD = 1080, 1080, 50
 HBAR = 140
 CARD_H = 110
